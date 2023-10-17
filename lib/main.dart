3553d027805906896bef6ff2b53dd4f914ea6618
alfaisaldwi <alfaisaldwi@gmail.com>
2023-10-18 03:25:07 +0700

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:flutter/services.dart';
+
 import 'app/routes/app_pages.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   GetStorage.init();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then(
-    (value) => GetMaterialApp(
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(
+    GetMaterialApp(
       title: "Mall UKM Kota Cirebon",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
